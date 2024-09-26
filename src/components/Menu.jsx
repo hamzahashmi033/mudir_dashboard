96cb52dbdcaf096a41c577552de26f09ff9b6f0b
hamzahashmi033 <hashmihamza846@gmail.com>
2024-09-26 14:18:20 -0700

    Project has been completed

--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -1,23 +1,51 @@
-import { Menu, MenuButton, MenuList, MenuItem, Button } from '@chakra-ui/react'
-import { ChevronDownIcon } from '@chakra-ui/icons'
+import {
+  Menu,
+  MenuButton,
+  MenuList,
+  MenuItem,
+  Button,
+  Flex,
+} from "@chakra-ui/react";
+import { ChevronDownIcon } from "@chakra-ui/icons";
+import Image from "next/image";
 
 export default function BasicMenu() {
-    return (
-
-        <MenuList>
-            <MenuItem color={"black"} onClick={() => alert("Download clicked")}>
-                Download
-            </MenuItem>
-            <MenuItem color={"black"} onClick={() => alert("Create clicked")}>
-                Create a Copy
-            </MenuItem>
-            <MenuItem color={"black"} onClick={() => alert("Delete clicked")}>
-                Delete
-            </MenuItem>
-            <MenuItem color={"black"} onClick={() => alert("Forward clicked")}>
-                Forward
-            </MenuItem>
-        </MenuList>
-
-    )
+  return (
+    <MenuList>
+      <MenuItem color={"black"} onClick={() => alert("Download clicked")}>
+        <div className="p-2 mt-2 w-full  flex justify-center xxxs:space-x-4 lg:space-x-1 xl:space-x-2 items-center">
+          <Image
+            src={require("/public/assets/man3.png")}
+            width={30}
+            className="cover rounded-full"
+          />
+          <div className="">
+            <h1 className="text-black font-bold text-base xs:text-[16px] lg:text-[164x] xl:text-[18px]">
+              John Doe
+            </h1>
+            <p className="text-black text-sm xs:text-[14px] lg:text-[14px] xl:text-[16px]">
+              hamzahashmi@hotmail.com
+            </p>
+          </div>
+        </div>
+      </MenuItem>
+      <MenuItem color={"black"} onClick={() => alert("Create clicked")}>
+        <div className="p-2 mt-2 w-full  flex justify-center xxxs:space-x-4 lg:space-x-1 xl:space-x-2 items-center">
+          <Image
+            src={require("/public/assets/man1.avif")}
+            width={30}
+            className="cover rounded-full"
+          />
+          <div className="">
+            <h1 className="text-black font-bold text-base xs:text-[16px] lg:text-[164x] xl:text-[18px]">
+              Hamza Hashmi
+            </h1>
+            <p className="text-black text-sm xs:text-[14px] lg:text-[14px] xl:text-[16px]">
+              hashmihamza846@gmail.com
+            </p>
+          </div>
+        </div>
+      </MenuItem>
+    </MenuList>
+  );
 }
